020141005e4146ce30546fa2d7c95310959d60ff
Tony Pigossi <tonypigossi@gmail.com>
2020-02-28 15:53:41 -0300

    Segundo Commit, criação da tbl Evento do DB

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Evento
 
-# Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    list_display =  ('titulo', 'data_evento', 'data_cricao')
+    list_filter = ('titulo', 'usuario', 'data_evento',)
+
+
+admin.site.register(Evento, EventoAdmin)
+
