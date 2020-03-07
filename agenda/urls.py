c01676f0411877ef96977e480b7419fa6e3ec8c1
Tony Pigossi <tonypigossi@gmail.com>
2020-03-07 20:08:14 -0300

    Inserção da opção de alterar senha e correções de bugs e melhorias de interface

--- agenda/urls.py
+++ agenda/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -28,5 +29,15 @@ urlpatterns = [
     path('agenda/evento/delete/<int:id_evento>', views.evento_delete, name='evento_delete'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('user_create', views.user_create, name='user_create'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+        name='password_change_done'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+        name='password_change'),
+    # path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+    #  name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+    #  name='password_reset_complete'),
     path('', RedirectView.as_view(url='agenda/')),
 ]
