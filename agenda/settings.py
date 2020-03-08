63a3a719861e347df657495db2cc0abb592fc76b
tonypigossim <tonypigossi@gmail.com>
2020-03-08 18:20:56 -0300

    Logo do site e ajuste do tamanho do calendário

--- agenda/settings.py
+++ agenda/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhosto', 'upagenda.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'upagenda.herokuapp.com']
 
 
 # Application definition
